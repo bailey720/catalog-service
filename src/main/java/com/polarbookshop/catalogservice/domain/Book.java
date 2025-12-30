dd12eff5d45230407495d2cdf1d325e0de86f629
swemeister <swemeister@outlook.com>
2025-12-29 17:34:37 -0700

    completed chapter 5

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -15,7 +15,7 @@ public record Book(
         @Id
         Long id,
 
-        @NotBlank(message = "The book ISBN bust be defined.")
+        @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(
             regexp = "^([0-9]{10}|[0-9]{13})$",
             message = "The ISBN format must be valid."
@@ -32,6 +32,8 @@ public record Book(
         @Positive(message = "The book price must be greater than zero.")
         Double price,
 
+        String publisher,
+
         @CreatedDate
         Instant createdDate,
 
@@ -41,7 +43,7 @@ public record Book(
         @Version
         int version
 ) {
-        public static Book of(String isbn, String title, String author, Double price) {
-                return new Book(null, isbn, title, author, price, null, null, 0);
+        public static Book of(String isbn, String title, String author, Double price, String publisher) {
+                return new Book(null, isbn, title, author, price, publisher, null, null, 0);
         }
 }
